fad35290abbb9236d73a28a04896a0ab8f10d957
Aditya Pawar <149230860+AdityaByte@users.noreply.github.com>
2023-11-13 21:08:04 +0530

    Add files via upload

---
+++ CommentsInJava.java
@@ -0,0 +1,14 @@
+// Comments in Java 
+// Java ignore the lines starting with these symbols // and /* */
+
+public class CommentsInJava {
+    public static void main(String[] args) {
+        System.out.println("Comments in Java - ");
+        System.out.println("One liner comment in java starts with double forward slash - //");
+        // like this
+        System.out.println("Multi line comment starts with /* and ends with */");
+        /* like 
+         * this
+         */
+    }
+}
