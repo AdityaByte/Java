d4a7d96250b679c10e7075b791d739b7f648bd67
Aditya Pawar <149230860+AdityaByte@users.noreply.github.com>
2023-11-13 20:48:30 +0530

    Update HelloWorldProgram.java

--- HelloWorldProgram.java
+++ HelloWorldProgram.java
@@ -1,5 +1,9 @@
+package Java; //optional
+
+// Java has class and function the class name should starts with a capital letter i.e., PascalConvention and it should be same with the file name .
+// And the class has a function named main and void means null .
 public class HelloWorldProgram {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World"); // print statement in java
     }
 }
