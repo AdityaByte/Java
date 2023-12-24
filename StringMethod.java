38486d6db53c200d5191d7d0fc06c15d3c6c90b4
Aditya Pawar <149230860+AdityaByte@users.noreply.github.com>
2023-12-24 22:33:38 +0530

    Update StringMethod.java

--- StringMethod.java
+++ StringMethod.java
@@ -25,9 +25,9 @@ public class StringMethod{
     String something1 = "     aditya     ";
     System.out.println(something1.trim()); // it removes unusal spaces in a string
     // it returns aditya
-    System.out.println(something.replace('t' , 'y'); // it replaces a character to the another character
+    System.out.println(something.replace('t' , 'y')); // it replaces a character to the another character
     // it returns adiyya
-    System.out.println(something.replace("adi" , "ida"); // it replaces a sequcence of chars to another sequence of char
+    System.out.println(something.replace("adi" , "ida")); // it replaces a sequcence of chars to another sequence of char
 
     System.out.println(something.startsWith("adi")); // it returns a boolean value
     // it return true in this case
@@ -35,13 +35,13 @@ public class StringMethod{
     // it returns false in this case
     System.out.println(something.indexOf("i")); // it returns the place of the particular charcter or string
     // it returns 2 bcz java reads from 0
-    System.out.println((something.indexOf("w" , 2)); // it starts from 2 and then gives the place of d
+    System.out.println(something.indexOf("w" , 2)); // it starts from 2 and then gives the place of d
     // it returns 5 i think
-    System.out.println(something.lastIndexOf("i");
+    System.out.println(something.lastIndexOf("i"));
     // it returns i dont know run it and check
-    System.out.println(something.subString(2)); // it forwards a substring which starts from 2 place 
+    System.out.println(something.substring(2)); // it forwards a substring which starts from 2 place 
     // it returns ityapawar
-    System.out.println(something.subString(2 , 5)); // it includes 2 and excludes 5
+    System.out.println(something.substring(2 , 5)); // it includes 2 and excludes 5
 
     System.out.println(something.charAt(5)); // it returns the charcter at the place specified
 
