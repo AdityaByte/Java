75c44a20037ce864e67b6b6508ab7397643c7fed
Aditya Pawar <149230860+AdityaByte@users.noreply.github.com>
2024-02-28 19:01:24 +0530

    Update SwitchCase.java

--- SwitchCase.java
+++ SwitchCase.java
@@ -3,44 +3,56 @@
 // NOTE ->
 // Here the code takes input as the byte value and according to the cases it gives output
 // Use break; statement for ending the case and coming back
-// SwitchCase is a static context so non static variable cannot be referred from a static context -- important note
+// If you dont make it static - scanner class this will throw an error -> "non-static variable s cannot be referenced from a static context"
 
 import java.util.Scanner;
 
-public class SwitchCase{
+class ScannerClass{
+    static Scanner s = new Scanner(System.in);
+}
 
-    static Scanner s = new Scanner(System.in); 
-
-    public static void main(String[] args) {
-        System.out.print("Enter the number => ");
-        byte number = s.nextByte();
-        byte num = number;
-
-        switch (num){
-            case 1:
-                System.out.println("Monday");
+class Example1{
+    static void enterCodeExample(){
+        System.out.print("Hey Buddy Enter the code for accessing the library -> ");
+        int code = ScannerClass.s.nextInt();
+        switch (code){
+            case 2001:
+                System.out.println("hey you got spell library access , congrats !");
                 break;
-            case 2:
-                System.out.println("Tuesday");
-                break;
-            case 3:
-                System.out.println("Wednesday");
-                break;
-            case 4:
-                System.out.println("Thrusday");
-                break;
-            case 5:
-                System.out.println("Friday");
-                break;
-            case 6:
-                System.out.println("Saturday");
-                break;
-            case 7:
-                System.out.println("Sunday");
+            case 2004:
+                System.out.println("Hey you got potion making books access , congrats!");
                 break;
             default:
-                System.out.println("please enter the number value from 1 to 7");
-
+                System.out.println("You have entered wrong code now Expelliarmus !!");
         }
+    }
+}
+
+class Example2{
+    static void enterPassCode(){
+        System.out.print("enter the passcode -> ");
+        String passcode = ScannerClass.s.next();
+        switch(passcode){
+            case "hogwarts":
+                System.out.println("welcome to hogwarts");
+                break;
+            case "harry":
+                System.out.println("Hey harry how are you seeing you after a long time");
+                break;
+            case "draco":
+                System.out.println("Draco malfoy.... :)");
+                break;
+            default:
+                System.out.println("Wrong passcode Avada Kedavra");
+        }
+    }
+}
+
+
+
+public class Test{
+    public static void main(String[] args) {
+        Example1.enterCodeExample();
+        Example2.enterPassCode();
     }
 }
