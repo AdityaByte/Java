4947aa2f73b56ca8f4cca12800e378d3f6104340
Aditya Pawar <149230860+AdityaByte@users.noreply.github.com>
2024-04-03 19:50:17 +0530

    Create constructor.java

---
+++ OOP/constructor.java
@@ -0,0 +1,40 @@
+// rename this file to Main.java before running it to your system
+// here in this code we learn about how the object is made , what is referance variable , what is instance variable ?
+// => ClassName(Class can be called as a datatype-nonprimitive) reference_variable = new(keyword - which makes the object in runtime) ClassName()- this is generally a function called as constructor ;
+// how the constructor made how it works ?
+// => Constructors can be made by java itself or we can put out some values to it and make it functionable
+// what is the this keyword ?
+// => this keyword is generally refers to the local variables of the class or it rename it by the object who it was calling it
+// what is the dot(.) seperator and how it works for declaring or calling out a function inside a class with the help of object ?
+
+public class Main{
+    public static void main(String[] args){
+
+      // first object
+        Student aditya = new Student(12, "aditya", 89.9f);
+        // System.out.println(aditya.toString());
+        System.out.println(aditya.rollNum);
+        System.out.println(aditya.name);
+        System.out.println(aditya.marks);
+
+      // second object
+        Student rahul = new Student(14, "rahul", 67.9f);
+        System.out.println(rahul.rollNum);
+        System.out.println(rahul.name);
+        System.out.println(rahul.marks);
+    }
+}
+
+// This java class is working as a template for the object
+class Student{
+    int rollNum;
+    String name;
+    float marks;
+    
+    // here is the constructor
+    Student (int rno, String name, float marks) {
+        this.rollNum = rno;
+        this.name = name;
+        this.marks = marks;
+    }
+}
